2c1445d67e2b5d70cb2eb72ce0d0d255bc9d336f
NiLuJe <ninuje@gmail.com>
2021-10-30 19:52:03 +0200

    Okay, LuaJIT doesn't like it when we push stuff to the C namespace.

--- select_h.lua
+++ select_h.lua
@@ -12,35 +12,6 @@ static const int POLLRDNORM = 64;
 static const int POLLRDBAND = 128;
 ]]
 
--- Most of this is handled via macros in C...
-local __NFDBITS = 8 * ffi.sizeof("__fd_mask")
-local function __FD_ELT(d)
-    return math.floor(d / __NFDBITS)
-end
-local function __FD_MASK(d)
-    return ffi.cast("__fd_mask", bit.lshift(1, d % __NFDBITS))
-end
-
-function C.FD_ZERO(s)
-    for i = 0, ffi.sizeof("fd_set") / ffi.sizeof("__fd_mask") do
-        s.__fds_bits[i] = 0
-    end
-end
-
-function C.FD_SET(d, s)
-    local fd_idx = __FD_ELT(d)
-    s.__fds_bits[fd_idx] = bit.bor(s.__fds_bits[fd_idx], __FD_MASK(d))
-end
-
-function C.FD_CLR(d, s)
-    local fd_idx = __FD_ELT(d)
-    s.__fds_bits[fd_idx] = bit.band(s.__fds_bits[fd_idx], bit.bnot(__FD_MASK(d)))
-end
-
-function C.FD_ISSET(d, s)
-    local fd_idx = __FD_ELT(d)
-    return bit.band(s.__fds_bits[fd_idx], __FD_MASK(d)) ~= 0
-end
 
 --[[
 #define __NFDBITS    (8 * (int) sizeof (__fd_mask))
@@ -63,6 +34,3 @@ end
 #define __FD_ISSET(d, s) \
   ((__FDS_BITS (s)[__FD_ELT (d)] & __FD_MASK (d)) != 0)
 --]]
-
--- To match FD_ISSET behavior with poll
-C.POLLIN_SET = bit.bor(C.POLLRDNORM, C.POLLRDBAND, C.POLLIN, C.POLLHUP, C.POLLERR)
--- socket.lua
+++ socket.lua
@@ -18,6 +18,41 @@ end
 
 local sockaddr_pt = ffi.typeof("struct sockaddr *")
 
+-- Most of this is handled via macros in C...
+--[[
+local __NFDBITS = 8 * ffi.sizeof("__fd_mask")
+local function __FD_ELT(d)
+    return math.floor(d / __NFDBITS)
+end
+local function __FD_MASK(d)
+    return ffi.cast("__fd_mask", bit.lshift(1, d % __NFDBITS))
+end
+
+local function FD_ZERO(s)
+    for i = 0, ffi.sizeof("fd_set") / ffi.sizeof("__fd_mask") do
+        s.__fds_bits[i] = 0
+    end
+end
+
+local function FD_SET(d, s)
+    local fd_idx = __FD_ELT(d)
+    s.__fds_bits[fd_idx] = bit.bor(s.__fds_bits[fd_idx], __FD_MASK(d))
+end
+
+local function FD_CLR(d, s)
+    local fd_idx = __FD_ELT(d)
+    s.__fds_bits[fd_idx] = bit.band(s.__fds_bits[fd_idx], bit.bnot(__FD_MASK(d)))
+end
+
+local function FD_ISSET(d, s)
+    local fd_idx = __FD_ELT(d)
+    return bit.band(s.__fds_bits[fd_idx], __FD_MASK(d)) ~= 0
+end
+--]]
+
+-- To match FD_ISSET behavior with poll
+local POLLIN_SET = bit.bor(C.POLLRDNORM, C.POLLRDBAND, C.POLLIN, C.POLLHUP, C.POLLERR)
+
 local Socket = {
     AF_UNIX = C.AF_UNIX,
     SOCK_DGRAM = C.SOCK_DGRAM,
@@ -93,7 +128,7 @@ function Socket.__index:recvAll(flags, event_queue)
                 return nil, re
             end
         elseif re > 0 then
-            if bit.band(evs[0].revents, C.POLLIN_SET) ~= 0 then
+            if bit.band(evs[0].revents, POLLIN_SET) ~= 0 then
                 local data
                 data, re = self:recv(buf, buf_len, flags)
                 full_buf_len = full_buf_len + re
