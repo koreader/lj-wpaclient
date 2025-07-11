6c2ac52758f599bf81f9ae8d4350cd9986797373
Rafal Kupka <r.kupson@gmail.com>
2025-07-11 14:06:14 +0100

    MIPS architecture support
    Adds support for MIPS architecture:
    
    ```
    LuaJIT 2.1.0-beta3 -- Copyright (C) 2005-2017 Mike Pall. http://luajit.org/
    JIT: ON MIPS32R2 fold cse dce fwd dse narrow loop abc sink fuse
    > WpaClient = require('wpaclient')
    > wcli, err = WpaClient.new('/var/run/wpa_supplicant/wwan0')
    > print(err)
    Failed to initialize socket instance
    ```
    
    The problem was clearly visible in the `strace`:
    
    ```
    strace: Process 5526 attached
    ...
    socket(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC|0x800, 0) = -1 EINVAL (Invalid argument)
    socket(AF_UNIX, SOCK_STREAM|0x800 /* SOCK_??? */, 0) = -1 EINVAL (Invalid argument)
    ```

--- consts_h.lua
+++ consts_h.lua
@@ -1,11 +1,16 @@
+local cur_path = (...):match("(.-)[^%(.|/)]+$")
 local ffi = require("ffi")
 
+-- Handle arch-dependent typedefs...
+if ffi.arch == "mips" then
+    require(cur_path .. "consts_mips_h")
+else
+    require(cur_path .. "consts_def_h")
+end
+
 pcall(ffi.cdef, "static const int AF_UNIX = 1;")
-pcall(ffi.cdef, "static const int SOCK_DGRAM = 2;")
-pcall(ffi.cdef, "static const int SOCK_NONBLOCK = 2048;")
 pcall(ffi.cdef, "static const int SOCK_CLOEXEC = 524288;")
 pcall(ffi.cdef, "static const int MSG_PEEK = 2;")
 pcall(ffi.cdef, "static const int MSG_NOSIGNAL = 16384;")
 pcall(ffi.cdef, "static const int EINTR = 4;")
 pcall(ffi.cdef, "static const int EAGAIN = 11;")
-pcall(ffi.cdef, "static const int EISCONN = 106;")
