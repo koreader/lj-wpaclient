939a19155753ab7d9b8a947495138f7221193805
Rafal Kupka <r.kupson@gmail.com>
2025-07-14 16:23:28 +0100

    Remove comments related to select(2) syscall

--- select_h.lua
+++ select_h.lua
@@ -10,26 +10,3 @@ pcall(ffi.cdef, "int select(int, fd_set *restrict, fd_set *restrict, fd_set *res
 
 pcall(ffi.cdef, "static const int POLLRDNORM = 64;")
 pcall(ffi.cdef, "static const int POLLRDBAND = 128;")
-
-
---[[
-#define __NFDBITS    (8 * (int) sizeof (__fd_mask))
-#define>__FD_ELT(d)  ((d) / __NFDBITS)
-#define>__FD_MASK(d) ((__fd_mask) (1UL << ((d) % __NFDBITS)))
-
-#define __FDS_BITS(set) ((set)->__fds_bits)
-
-#define __FD_ZERO(s) \
-  do {                                                                        \
-    unsigned int __i;                                                         \
-    fd_set *__arr = (s);                                                      \
-    for (__i = 0; __i < sizeof (fd_set) / sizeof (__fd_mask); ++__i)          \
-      __FDS_BITS (__arr)[__i] = 0;                                            \
-  } while (0)
-#define __FD_SET(d, s) \
-  ((void) (__FDS_BITS (s)[__FD_ELT(d)] |= __FD_MASK(d)))
-#define __FD_CLR(d, s) \
-  ((void) (__FDS_BITS (s)[__FD_ELT(d)] &= ~__FD_MASK(d)))
-#define __FD_ISSET(d, s) \
-  ((__FDS_BITS (s)[__FD_ELT (d)] & __FD_MASK (d)) != 0)
---]]
