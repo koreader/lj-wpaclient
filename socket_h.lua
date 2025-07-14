4b83776baee2f0d2268a52f6c2b126fb48345207
Rafal Kupka <r.kupson@gmail.com>
2025-07-14 16:42:34 +0100

    More unused ffi.cdef() symbols

--- socket_h.lua
+++ socket_h.lua
@@ -9,7 +9,6 @@ struct sockaddr {
 pcall(ffi.cdef, "int socket(int, int, int) __attribute__((nothrow, leaf));")
 pcall(ffi.cdef, "int bind(int, const struct sockaddr *, unsigned int) __attribute__((nothrow, leaf));")
 pcall(ffi.cdef, "int connect(int, const struct sockaddr *, unsigned int);")
-pcall(ffi.cdef, "ssize_t recvfrom(int, void *restrict, size_t, int, struct sockaddr *restrict, unsigned int *restrict);")
 pcall(ffi.cdef, "ssize_t recv(int, void *, size_t, int);")
 pcall(ffi.cdef, "ssize_t send(int, const void *, size_t, int);")
 pcall(ffi.cdef, "int close(int);")
